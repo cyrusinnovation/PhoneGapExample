0086d5c2c7819c70df777e95b28130ef2ca3e784
Daniel Ramteke <dramteke@cyrusinnovation.com>
2012-08-02 11:23:58 -0400

    Added current position to map.

--- js/google_map.js
+++ js/google_map.js
@@ -1,11 +1,35 @@
 $(document).ready(function () {
+
+
     $('#page2').live("pageshow", function(){
         $('#map_canvas').gmap('refresh');
     });
 
     $('#page2').live("pageinit", function(){
-        var yourStartLatLng = new google.maps.LatLng(59.3426606750, 18.0736160278);
-        $('#map_canvas').gmap({'center':yourStartLatLng});
+        getCurrentPosition();
     });
+
+    function getCurrentPosition(){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    function updateMap(lat, long){
+        var yourStartLatLng = new google.maps.LatLng(lat, long);
+        $('#map_canvas').gmap({'center':yourStartLatLng});
+    }
+
+    function addMarker(lat, long){
+        $('#map_canvas').gmap('addMarker', { 'position': new google.maps.LatLng(lat,long) });
+    }
+
+    var onSuccess = function(position) {
+        updateMap(position.coords.latitude, position.coords.longitude);
+        addMarker(position.coords.latitude, position.coords.longitude);
+    };
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+    }
 
 });
