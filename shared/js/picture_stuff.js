f5c5fc1b36a0cc1020035d8dd736ca34684314ce
Daniel Ramteke <dramteke@cyrusinnovation.com>
2012-08-03 11:34:39 -0400

    Fixed the picture taking function.

--- shared/js/picture_stuff.js
+++ shared/js/picture_stuff.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
     // Page 3 - picture sharing
-    $('#select_picture').click(function(){
+
+    $('#take_picture').click(function(){
         navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
             destinationType: Camera.DestinationType.DATA_URL
         });
     });
 
-
     function onSuccess(imageData) {
+        $.mobile.changePage($('#page4'),{transition: 'slide'});
         var image = document.getElementById('myImage');
         image.src = "data:image/jpeg;base64," + imageData;
     }
