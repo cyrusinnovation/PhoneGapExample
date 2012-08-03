1d0c98a56f558d57354a6b9ca09cd39468cdde49
Daniel Ramteke <dramteke@cyrusinnovation.com>
2012-08-03 12:02:39 -0400

    Added select from photo album. Fixing onFail method.

--- shared/js/picture_stuff.js
+++ shared/js/picture_stuff.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
     // Page 3 - picture sharing
 
-    $('#take_picture').click(function(){
-        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-            destinationType: Camera.DestinationType.DATA_URL
-        });
+    $('#take_picture').click(function () {
+        navigator.camera.getPicture(onPhotoURISuccess, onCancelOrFail,
+            {
+                quality:50,
+                destinationType: Camera.DestinationType.FILE_URI
+            }
+        );
     });
 
-    function onSuccess(imageData) {
-        $.mobile.changePage($('#page4'),{transition: 'slide'});
+    $('#select_picture').click(function () {
+        navigator.camera.getPicture(onPhotoURISuccess, onCancelOrFail,
+            {
+                quality:50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+            }
+        );
+    });
+
+    function onPhotoURISuccess(imageURI) {
+        $.mobile.changePage($('#page4'), {transition:'slide'});
         var image = document.getElementById('myImage');
-        image.src = "data:image/jpeg;base64," + imageData;
+        image.src = imageURI;
     }
 
-    function onFail(message) {
+    function onCancelOrFail(message) {
+        if(message === "no image selected") return;
+
         alert('Failed because: ' + message);
     }
-
 });
